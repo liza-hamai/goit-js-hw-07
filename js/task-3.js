afab4d0e9a2fd47c2a434889445784cca5f5f8b8
liza-hamai <elizaveta.hamai@knu.ua>
2025-10-30 01:46:22 +0200

    add changes

--- js/task-3.js
+++ js/task-3.js
@@ -2,6 +2,6 @@ const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 input.addEventListener("input", (event) => {
-    const value = event.currentTarget.value;
+    const value = event.currentTarget.value.trim();
     output.textContent = value === "" ? "Anonymous" : value;
 });
--- js/task-4.js
+++ js/task-4.js
@@ -8,7 +8,7 @@ function submitFunction(event) {
     const password = form.elements.password.value;
 
     if (email === "" || password === "") {
-        return console.log("All form fields must be filled in");
+        return alert("All form fields must be filled in");
     };
 
     const formData = {
