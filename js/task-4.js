78332b16bfe477959ec65860d42ae15204227327
liza-hamai <elizaveta.hamai@knu.ua>
2025-10-31 03:31:01 +0200

    add changes

--- js/task-4.js
+++ js/task-4.js
@@ -4,16 +4,16 @@ registerForm.addEventListener("submit", submitFunction);
 function submitFunction(event) {
     event.preventDefault();
     const form = event.target;
-    const email = form.elements.email.value;
-    const password = form.elements.password.value;
+    const email = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
 
     if (email === "" || password === "") {
         return alert("All form fields must be filled in");
     };
 
     const formData = {
-        email : email.trim(),
-        password : password.trim(),
+        email : email,
+        password : password,
     };
 
     console.log(formData);
